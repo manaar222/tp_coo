c0f2b9b92f9d9b19f09eac1120020c86599d289b
Manar <manar.chikhi@univ-tlse3.fr>
2026-09-23 10:12:12 +0200

    pre-commit run -a

--- velos/manage.py
+++ velos/manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boissons.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "velos.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- velos/velos/asgi.py
+++ velos/velos/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for boissons project.
+ASGI config for velos project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boissons.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "velos.settings")
 
 application = get_asgi_application()
